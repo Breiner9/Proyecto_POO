82aead9ce376dc0979c49970056b970f6b102b15
Breiner9 <breinerpinilla9@gmail.com>
2026-04-29 19:12:05 +0000

    Agrego clases motor y coche (algunos cambios)

--- clases/coche.py
+++ clases/coche.py
@@ -3,17 +3,20 @@ from clases.vehiculo import Vehiculo
 
 #Creamos la clase coche heredando atributos de vehiculo
 class Coche(Vehiculo):
-        def __init__(self,marca,modelo,anio):
+        def __init__(self,marca,modelo,anio, motor):
             super().__init__()
             self.marca = marca
             self.modelo = modelo 
             self.anio = anio 
-            
+            self.motor = motor 
+
 #Creamos el método describir       
         def describir(self):
             print(f"Marca: {self.marca}")
             print(f"Modelo: {self.modelo}")
             print(f"Anio: {self.anio}")
+            print(f"Potencia Motor: {self.motor.potencia}")
+            print(f"Tipo Motor: {self.motor.tipo}")
 
 #Creamos el método acelerar para coche
         def acelerar(self):
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from clases.vehiculo import Vehiculo
+from clases.motor import Motor
 from clases.coche import Coche
 from clases.bicicleta import Bicicleta
 #NOTA: no imprimimos acelerar en coche ni en bici porque lo hacemos en la lista para que no se duplique la velocidad
@@ -8,10 +9,14 @@ vehiculo1 = Vehiculo()
 print(f"Velocidad Inicial: {vehiculo1.velocidad}")
 vehiculo1.acelerar()
 print(f"Velocidad Actual: {vehiculo1.velocidad}\n")
+ 
+
+#Creamos un objeto para motor
+motor1 = Motor("283 CV","Electrico")
 
 #Creamos un objeto para coche 
 
-coche1 = Coche("Tesla", "Model 3", 2024)
+coche1 = Coche("Tesla", "Model 3", 2024, motor1)
 
 coche1.describir()
 print(f"Velocidad: {coche1.velocidad}km/h\n")
